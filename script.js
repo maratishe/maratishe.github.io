5b7966361fcddf459a007941d02968419aee4b5d
Marat Zhanikeev <maratishe@gmail.com>
2014-10-29 21:49:40 +0900

    commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $( document).ready( function() { 
+	var share = new Share( '#sharebuttons', { 
+		title: $.trim( $( 'title').text()),
+		description: 'Migrated to GithubPages. First time to have full control over what the blog looks and behaves like. http://maratishe.github.io',
+		ui: { flyout: 'bottom left'},
+		networks: { 
+			google_plus: { enabled: true, url: document.location.href},
+			twitter: { enabled: true},
+			facebook: { enabled: true},
+			linkedin: { url: document.location.href}, 
+			email: { enabled: true, to: 'maratishe@gmail.com', title: 'Comment to ' + $.trim( $( 'title').text()), description: "Marat, \n\n Concerning your GithubPage '" + $.trim( $( 'title').text()) + "'...\n\n"}
+		}
+		
+	})
 	var one = function( $box) { $box.click( function() { 
 		var tag = $box.find( 'span').get().length ? $.trim( $box.find( 'span').first().text()) : $.trim( $box.text());  
 		$( 'div[id="links"]').children().each( function() { 
